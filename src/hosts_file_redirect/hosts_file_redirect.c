e1b1599609bc747871f92fa700e416877a467d3f
tianqwe <98233005+tianqwe@users.noreply.github.com>
2024-06-08 19:52:19 +0800

    更新 hosts_file_redirect.c

--- src/hosts_file_redirect/hosts_file_redirect.c
+++ src/hosts_file_redirect/hosts_file_redirect.c
@@ -4,136 +4,112 @@
  * Copyright (C) 2024 skkk. All Rights Reserved.
  */
 
-#include <linux/err.h>
-#include <linux/fs.h>
+#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/printk.h>
-#include <linux/string.h>
+#include <linux/init.h>
+#include <linux/kallsyms.h>
+#include <linux/ptrace.h>
+#include <linux/unistd.h>
+#include <asm/ptrace.h>
 
 #include <kpm_utils.h>
 #include <kpm_hook_utils.h>
 
-KPM_NAME("hosts_file_redirect");
-KPM_VERSION(HFR_VERSION);
+KPM_NAME("page_fault_bypass");
+KPM_VERSION("1.0");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("skkk");
-KPM_DESCRIPTION("hosts file redirect: /data/adb/hosts");
+KPM_DESCRIPTION("Bypass page fault detection");
 
-static const char hostsOrigPath[] = "/system/etc/hosts";
-static const char hostsRedirectPath[] = "/data/adb/hosts";
+typedef void (*do_page_fault_t)(struct pt_regs *, unsigned long);
+static do_page_fault_t original_do_page_fault;
 
-struct open_flags;
-hook_func_def(do_filp_open, struct file *, int dfd, struct filename *pathname, const struct open_flags *o);
-hook_func_no_info(do_filp_open);
+hook_func_def(do_page_fault, void, struct pt_regs *regs, unsigned long error_code);
+hook_func_no_info(do_page_fault);
 
-static struct file *hook_replace(do_filp_open)(int dfd, struct filename *pathname, const struct open_flags *o)
-{
-    if (unlikely(!strcmp(hostsOrigPath, pathname->name))) {
-        bool replace = false;
-        const char *originName = NULL;
-        // netd is the root user
-        if (current_uid() == 0) {
-            replace = true;
-            originName = pathname->name;
-            pathname->name = hostsRedirectPath;
-            set_priv_selinx_allow(1);
-        }
-
-        struct file *f = hook_call_backup(do_filp_open, dfd, pathname, o);
-
-        if (replace) {
-            set_priv_selinx_allow(0);
-            pathname->name = originName;
-            //retry origin
-            if (IS_ERR(f)) f = hook_call_backup(do_filp_open, dfd, pathname, o);
-        }
-        return f;
-    }
-    return hook_call_backup(do_filp_open, dfd, pathname, o);
+static void hook_replace(do_page_fault)(struct pt_regs *regs, unsigned long error_code) {
+    printk(KERN_INFO "Bypassing page fault detection\n");
+    // Optionally handle the fault here
 }
 
-static inline bool installHook()
-{
+static inline bool installHook() {
     bool ret = false;
 
-    if (!hook_success(do_filp_open)) {
-        hook_install(do_filp_open);
-        if (!hook_success(do_filp_open)) goto exit;
-        pr_info("HFR: hosts file is: '%s'\n", hostsRedirectPath);
-        pr_info("HFR: enabled !\n");
-    } else {
-        pr_info("HFR: Always enabled !\n");
+    original_do_page_fault = (do_page_fault_t)kallsyms_lookup_name("do_page_fault");
+    if (!original_do_page_fault) {
+        printk(KERN_ERR "Could not find do_page_fault symbol\n");
+        return false;
     }
+
+    hook_install(do_page_fault);
+    if (!hook_success(do_page_fault)) {
+        printk(KERN_ERR "Failed to install do_page_fault hook\n");
+        return false;
+    }
+
+    printk(KERN_INFO "Page fault detection hook installed\n");
     ret = true;
 
-exit:
     return ret;
 }
 
-static inline bool uninstallHook()
-{
-    if (hook_success(do_filp_open)) {
-        unhook((void *)hook_original(do_filp_open));
-        hook_err(do_filp_open) = HOOK_NOT_HOOK;
-        pr_info("HFR: disbaled !\n");
+static inline bool uninstallHook() {
+    if (hook_success(do_page_fault)) {
+        unhook((void *)hook_original(do_page_fault));
+        hook_err(do_page_fault) = HOOK_NOT_HOOK;
+        printk(KERN_INFO "Page fault detection hook removed\n");
     } else {
-        pr_info("HFR: Always disabled !\n");
+        printk(KERN_INFO "Page fault detection hook was not installed\n");
     }
     return true;
 }
 
-static inline void printInfo()
-{
-    pr_info("Kernel Version: %x\n", kver);
-    pr_info("Kernel Patch Version: %x\n", kpver);
+static inline void printInfo() {
+    printk(KERN_INFO "Kernel Version: %x\n", kver);
+    printk(KERN_INFO "Kernel Patch Version: %x\n", kpver);
 }
 
-static inline bool hfr_control(bool enable)
-{
+static inline bool pf_control(bool enable) {
     return enable ? installHook() : uninstallHook();
 }
 
-static long hosts_file_redirect_init(const char *args, const char *event, void *__user reserved)
-{
+static long page_fault_bypass_init(const char *args, const char *event, void *__user reserved) {
     long ret = 0;
 
     printInfo();
-    pr_info("HFR: initializing ...\n");
+    printk(KERN_INFO "Initializing page fault bypass...\n");
 
-    if (hfr_control(true)) {
-        pr_info("HFR: Initialization successful!\n");
-        goto exit;
+    if (pf_control(true)) {
+        printk(KERN_INFO "Initialization successful!\n");
+    } else {
+        ret = 1;
+        printk(KERN_INFO "Initialization failed!\n");
     }
 
-    ret = 1;
-    pr_info("HFR: initializing failed!\n");
-exit:
     return ret;
 }
 
-static long hosts_file_redirect_control0(const char *args, char *__user out_msg, int outlen)
-{
+static long page_fault_bypass_control0(const char *args, char *__user out_msg, int outlen) {
     if (args) {
         if (strncmp(args, "enable", 6) == 0) {
-            writeOutMsg(out_msg, &outlen, hfr_control(true) ? "HFR: enabled !" : "HFR: enable fail !");
+            writeOutMsg(out_msg, &outlen, pf_control(true) ? "Page fault bypass enabled!" : "Enable failed!");
         } else if (strncmp(args, "disable", 7) == 0) {
-            writeOutMsg(out_msg, &outlen, hfr_control(false) ? "HFR: disbaled !" : "HFR: disbale fail !");
+            writeOutMsg(out_msg, &outlen, pf_control(false) ? "Page fault bypass disabled!" : "Disable failed!");
         } else {
-            pr_info("HFR: ctl error, args=%s\n", args);
-            writeOutMsg(out_msg, &outlen, "HFR: ctl error !");
+            printk(KERN_INFO "Control error, args=%s\n", args);
+            writeOutMsg(out_msg, &outlen, "Control error!");
             return -1;
         }
     }
     return 0;
 }
 
-static long hosts_file_redirect_exit(void *__user reserved)
-{
+static long page_fault_bypass_exit(void *__user reserved) {
     uninstallHook();
-    pr_info("HFR: Exiting ...\n");
+    printk(KERN_INFO "Exiting page fault bypass...\n");
     return 0;
 }
 
-KPM_INIT(hosts_file_redirect_init);
-KPM_CTL0(hosts_file_redirect_control0);
-KPM_EXIT(hosts_file_redirect_exit);
+KPM_INIT(page_fault_bypass_init);
+KPM_CTL0(page_fault_bypass_control0);
+KPM_EXIT(page_fault_bypass_exit);
